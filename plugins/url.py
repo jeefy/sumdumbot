ce051bc21e921690f61ffd0e85f18e2471cf00c5
metal-slime <josephkkelly@gmail.com>
2012-07-23 12:06:11 -0300

    Adding in title targeting adjustment

--- plugins/url.py
+++ plugins/url.py
@@ -12,7 +12,7 @@ def match(msg):
         httpreq = urllib.urlopen(match)
         info    =  httpreq.info()
         if info['Content-Type'].find('text/html') != -1:
-            title   = BeautifulSoup(httpreq).title.string
+            title   = BeautifulSoup(httpreq).html.head.title.string
             if title is not None:
                 toSend =  BeautifulSoup(title).__str__('utf-8').replace('\r', '').replace('\n', '').strip()
                 toSend =  BeautifulStoneSoup(toSend,convertEntities=BeautifulStoneSoup.HTML_ENTITIES).contents[0]
