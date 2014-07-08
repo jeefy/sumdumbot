528ad58e62f2a32aea662113351aaa88e9b53da4
Jeffrey Sica <jeef111x@gmail.com>
2014-07-08 16:00:13 -0400

    Indentation issues, Sublime so dumb...

--- app/lib/plugins/dice.py
+++ app/lib/plugins/dice.py
@@ -8,9 +8,9 @@ helloMatch = re.compile('\!roll\s*(\d*)?', flags=re.IGNORECASE)
 
 def match(msg):
 	roll = helloMatch.match(msg.msg)
-    if roll is not None and msg.silent:
-    	try:
-    		sides = int(roll.group(1))
-    	except:
-    		sides = 20
-        msg.irc.msg(msg.channel, msg.source + " rolled a " + random.randint(1,sides))
+	if roll is not None and msg.silent:
+		try:
+			sides = int(roll.group(1))
+		except:
+			sides = 20
+		msg.irc.msg(msg.channel, msg.source + " rolled a " + random.randint(1,sides))
